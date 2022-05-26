4cc0d33c1d9c2cca6e5593395dd21a012407d8cf
Neo <89941741+MatrixBytes@users.noreply.github.com>
2022-05-26 15:55:32 +0200

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,23 +1,58 @@
-enc      = "ibnnribnnr"   
-key      = [1, 1, 2, 2, 3]
+from random import randint
 
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-dec      = ""
-keyloc   = 0
+def decrypt(enc:str, key:list):
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-for char in enc:
-    index = alphabet.index(char)
+    keyloc = 0
+    dec = ""
 
-    print(f"[Debug] {char} ({index} - {key[keyloc]}) -> ", end = "")
+    for char in enc: 
+        charindex = alphabet.index(char)
 
-    decchar = alphabet[index - key[keyloc]]
-    print(decchar)
+        print(f"[Debug] {char} ({charindex} - {key[keyloc]}) -> ", end = "")
 
-    dec += decchar
+        decchar = alphabet[charindex - key[keyloc]]
+        print(decchar)
 
-    keyloc += 1
+        dec += decchar
 
-    if keyloc == len(key):
-        keyloc = 0
+        keyloc += 1
 
-print(dec)
+        if keyloc == len(key):
+            keyloc = 0
+
+    return dec
+
+def encrypt(dec:str, keylength = None, key = True):
+    if keylength == None:
+        keylength = len(dec)
+
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    keyloc = 0
+    enc = ""
+
+    if key:
+        key = [randint(1, 10) for _ in range(keylength)]
+
+    for char in dec:
+        charindex = alphabet.index(char)
+
+        print(f"[Debug] {char} ({charindex} + {key[keyloc]}) -> ", end = "")
+
+        encchar = alphabet[charindex + key[keyloc]]
+        print(encchar)
+
+
+        enc += encchar
+
+        keyloc += 1
+
+        if keyloc == len(key):
+            keyloc = 0
+    
+    return key, enc
+
+xkey, xenc = encrypt("hello")
+print(xkey, xenc)
+print(decrypt(xenc, xkey))
